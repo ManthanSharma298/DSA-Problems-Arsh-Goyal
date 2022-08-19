60d7b8a75316ba0fcfbdb77a22f362b3d0e5a483
ManthanSharma298 <manthansharma154@gmail.com>
2022-08-19 09:23:57 +0530

    updated files

--- Graphs/find-eventual-safe-states.cpp
+++ Graphs/find-eventual-safe-states.cpp
@@ -1,6 +1,6 @@
 
 
-// DFS: O(n), O(n+n)
+// DFS using map: O(n), O(n+n)
 class Solution {
     map<int, int> isSafe;
     vector<vector<int>> g;
@@ -30,6 +30,42 @@ public:
         for(auto e: isSafe){
             if(e.second) res.push_back(e.first);
         }
+        return res;
+    }
+};
+
+// 2. Using extra vector: O(n), O(n+n+n)
+class Solution {
+public:
+    void dfs(int node, vector<int>& trm, vector<int>& vis, vector<vector<int>>& g){
+        vis[node] = 1;
+        int f = 1;
+        for(int cld: g[node]){
+            if(!vis[cld]) dfs(cld, trm, vis, g);
+            if(trm[cld] == 0){
+                f = 0;
+                break;
+            }  
+        }
+        
+        trm[node] = f;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> trm(n, 0);
+        vector<int> vis(n, 0);
+        
+        for(int i=0; i<n; ++i){
+            if(!vis[i]){
+                dfs(i, trm, vis, graph);
+            }
+        }
+        
+        vector<int> res;
+        for(int i=0; i<n; ++i) {
+            if(trm[i]) res.push_back(i);
+        }
+        
         return res;
     }
 };
